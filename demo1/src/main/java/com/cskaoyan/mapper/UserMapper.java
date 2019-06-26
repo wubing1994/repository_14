697002c0003fa9580b35ec65ea1e85425709db5d
wubing1178 <837137118@qq.com>
2019-06-26 14:31:49 +0800

    update

--- demo1/src/main/java/com/cskaoyan/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cskaoyan.mapper;
-
-import com.cskaoyan.bean.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserMapper {
-
-    User queryUserById(@Param("id") int id);
-}
